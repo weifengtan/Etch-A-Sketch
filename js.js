cdbd4783008864f3a6ba391e50e169042c675718
Alan Tan <alan@Alans-MacBook-Pro-2.local>
2023-01-05 17:33:41 -0800

    made a grid using DOM manipulation

---
+++ js.js
@@ -0,0 +1,20 @@
+const grid_container = document.querySelector('.grid-container'); 
+let size = 0;
+size = 16;  
+let auto_str = ' auto'; 
+grid_container.style.gridTemplateColumns = `${auto_str.repeat(size)}`;
+
+
+
+
+for (let i = 0; i < size; i++){
+    for(let j = 0; j < size; j++){
+        const grid_div = document.createElement('div'); 
+        grid_div.setAttribute(
+            'style',
+            'border: 4px red solid; width: 10px; height: 10px'
+        )
+        grid_div.textContent = "1"; 
+        grid_container.appendChild(grid_div); 
+    }
+}
