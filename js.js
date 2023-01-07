c95fa66a34924b4e19909474238f8aa4fbe6f257
Alan <tan.alan2000@gmail.com>
2023-01-07 14:46:42 -0800

    fixed so it turns blank whenever a new size was entered

--- js.js
+++ js.js
@@ -1,14 +1,19 @@
 const grid_container = document.querySelector('.grid-container'); 
 const button_container = document.querySelector('.button-container'); 
 const confirm_button = document.querySelector('.size-container button');
-
+const size_output = document.querySelector('.size-container input'); 
 let size; 
 let auto_str = ' auto'; 
 
-
-confirm_button.addEventListener('click', () => {
+function run() {
+    grid_container.innerHTML = ''; 
     size = document.getElementById('size').value;
+    if (size > 100){
+        size = 100
+    }
+    console.log(size) 
     size = Number(size);  
+    
     grid_container.style.gridTemplateColumns = `${auto_str.repeat(size)}`;
     for (let i = 0; i < size; i++){
         for(let j = 0; j < size; j++){
@@ -22,26 +27,11 @@ confirm_button.addEventListener('click', () => {
             })
             grid_container.appendChild(grid_div); 
         }
-    }  
-})
-
-/*
-grid_container.style.gridTemplateColumns = `${auto_str.repeat(size)}`;
-for (let i = 0; i < size; i++){
-    for(let j = 0; j < size; j++){
-        const grid_div = document.createElement('div'); 
-        grid_div.addEventListener('mouseenter', () => {
-            // put if statement here
-                grid_div.setAttribute(
-                    'style',
-                    'background-color: black;'
-                )
-        })
-        grid_div.setAttribute(
-            'style',
-            'border: 4px solid red'
-        )
-        grid_container.appendChild(grid_div); 
     }
 }
-*/
+
+
+
+confirm_button.addEventListener('click', run);
+
+
